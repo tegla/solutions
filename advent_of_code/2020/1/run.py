7a0639e26a2f8585f981ff54a8c9d94bad7e1339
Peter Nagy <tegla@tegla.net>
2020-12-01 21:42:02 +0100

    second answer to day 1

--- advent_of_code/2020/1/run.py
+++ advent_of_code/2020/1/run.py
@@ -10,8 +10,26 @@ def first():
             print("First Answer: ", i*(2020-i))
         negs.add(2020-i)
 
+def second():
+    d = {}
+    small = filter(lambda x: x <1010, input())
+    for i in range(0, len(small)-1):
+        for j in range(i+1, len(small)):
+            d[2020-(small[i]+small[j])] = [small[i],small[j]]
+    big = filter(lambda x: x>=1010, input())
+    for i in big:
+        if i in d:
+            result = d[i]
+            result.append(i)
+            print(result)
+            mul = 1
+            for n in result:
+                mul*=n
+            print("Second Answer: ", mul)
+
 def main():
     first()
+    second()
 
 if __name__ == "__main__":
 	main()
