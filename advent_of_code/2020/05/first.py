b2c3875fd386109c1e26965cca61f7cdddad99e1
Peter Nagy <tegla@tegla.net>
2020-12-05 08:12:28 +0100

    day 5, first

---
+++ advent_of_code/2020/05/first.py
@@ -0,0 +1,26 @@
+from functools import reduce
+
+
+with open('/tmp/input.txt') as f:
+    ls = [l.rstrip() for l in f]
+
+def multiplier(c):
+    return 1 if c in {'B','R'} else 0
+
+def compute(s):
+    return reduce(lambda a,b:a*2+b, map(multiplier, s))
+
+def row(l):
+    return compute(l[:7])
+
+def col(l):
+    return compute(l[7:])
+
+def id(l):
+    return row(l) * 8 + col(l)
+
+assert id('BFFFBBFRRR') == 567
+assert id('FFFBBBFRRR') == 119
+assert id('BBFFBBFRLL') == 820
+
+print(max(map(id, ls)))
