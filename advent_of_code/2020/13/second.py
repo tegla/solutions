81573c09c4ceb082f19bb49c7f40ea86bb471861
Peter Nagy <tegla@tegla.net>
2020-12-13 10:45:18 +0100

    use Fermat's algo for inverse, instead of brute-force looping through

--- advent_of_code/2020/13/second.py
+++ advent_of_code/2020/13/second.py
@@ -1,5 +1,5 @@
 from functools import reduce
-with open('/tmp/example.txt') as f:
+with open('/tmp/input.txt') as f:
     ts = int(next(f))
     buses = [None if n == 'x' else int(n) for n in next(f).strip().split(',')]
 
@@ -12,15 +12,21 @@ for i in range(0, len(buses)):
 print(os)
 
 
+def inverse(a, p):
+    # Fermat's algorithm for modular inverse (all bus ids are prime)
+    res = 1
+    for i in range (0, p-2):
+        res*=a
+        res%=p
+    return res
+
 def common(b1, b2):
     print(b1, b2)
     m1, o1 = b1
     m2, o2 = b2
-    i = 0
-    while (m1*i+o1) % m2 != (m2-o2):
-        i += 1
+    i = (inverse(m1, m2)*(-o1-o2))%m2
     return (m1*m2, m1*i+o1)
 
 
 m, o = reduce(common, os)
-print(o)
+print("result: ", o)
