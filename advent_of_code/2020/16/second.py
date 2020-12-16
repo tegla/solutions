97ac3b987a837454e84f9c66b55729592ef3fed6
Peter Nagy <tegla@tegla.net>
2020-12-16 09:56:31 +0100

    reformatted, added an explanation comment, and switched to defaultdict for easier readability

--- advent_of_code/2020/16/second.py
+++ advent_of_code/2020/16/second.py
@@ -2,11 +2,12 @@ from input import *
 
 import operator
 from functools import reduce
-import itertools
+from collections import defaultdict
+
 
 def validforfield(field, num):
     (_, l1, r1, l2, r2) = field
-    res = (l1 <= num and num <=r1) or (l2 <= num and num <=r2)
+    res = (l1 <= num and num <= r1) or (l2 <= num and num <= r2)
     #print(field, num, res)
     return res
 
@@ -14,33 +15,34 @@ def validforfield(field, num):
 def validforafield(num):
     return reduce(operator.or_, [validforfield(f, num) for f in fields])
 
+
 def validticket(t):
     return reduce(operator.and_, [validforafield(n) for n in t])
 
+
 theirs = [t for t in theirs if validticket(t)]
+
 
 def validforcol(field, col):
     return reduce(operator.and_, [validforfield(field, t[col]) for t in theirs])
 
-validcols = []
+
+validcols = defaultdict(set)
 for f in range(0, len(fields)):
-    v = set()
     for c in range(0, len(mine)):
         if validforcol(fields[f], c):
-            v.add(c)
-    #print(len(validcols),v)
-    validcols.append(v)
+            validcols[f].add(c)
 
-orderedvalidcols = list(zip(itertools.count(0), validcols))
-orderedvalidcols.sort(key=lambda x:len(x[1]))
-orderedvalidcols
+orderedvalidcols = list(validcols.items())
+orderedvalidcols.sort(key=lambda x: len(x[1]))
 
 seen = set()
-
 fieldmap = dict()
 
 for f, vc in orderedvalidcols:
     left = vc - seen
+    # This is a greedy search with no backtracking.
+    # So if there are more than one option, bail out.
     assert len(left) == 1
     c = left.pop()
     fieldmap[f] = c
@@ -50,6 +52,6 @@ res = 1
 for i in range(0, len(fields)):
     (name, _, _, _, _) = fields[i]
     if name.startswith("departure"):
-        res*=mine[fieldmap[i]]
+        res *= mine[fieldmap[i]]
 
 print(res)
