5f0f4e3588b2a4bb153de0b0baa2cbd11b5c9f73
Peter Nagy <tegla@tegla.net>
2020-12-19 09:11:50 +0100

    day 19, second puzzle: very dumb CYK algorithm

---
+++ advent_of_code/2020/19/second.py
@@ -0,0 +1,71 @@
+from collections import defaultdict
+
+
+rules = dict()
+messages = []
+
+with open('/tmp/input.txt') as f:
+    for l in f:
+        l = l.strip()
+        if l == "":
+            break
+        n, rs = l.split(":")
+        rs = rs.strip()
+        n = int(n)
+        if rs[0] == '"':
+            rs = rs[1]
+        else:
+            rs = rs.strip().split("|")
+            rs = [list(map(int, r.strip().split(" "))) for r in rs]
+        rules[n] = rs
+
+    for l in f:
+        messages.append(l.strip())
+
+rules[8] = [[42], [42, 8]]
+rules[11] = [[42, 31], [42, 11, 31]]
+
+
+def goodmessage(m):
+    # string position, rule_nr -> lens
+    matches = defaultdict(set)
+
+    for n, rs in rules.items():
+        if isinstance(rs, str):
+            for i in range(0, len(m)):
+                if m[i: i+len(rs)] == rs:
+                    matches[(i, n)].add(len(rs))
+
+    def MatchRuleListLen(i, rl):
+        if len(rl) == 0:
+            yield 0
+            return
+        for l in matches[(i, rl[0])]:
+            for restlen in MatchRuleListLen(i+l, rl[1:]):
+                yield l + restlen
+
+    while True:
+        updated = False
+        for n, rs in rules.items():
+            if isinstance(rs, str):
+                continue
+            for rl in rs:
+                for i in range(0, len(m)):
+                    for l in MatchRuleListLen(i, rl):
+                        ls = matches[(i, n)]
+                        if not l in ls:
+                            updated = True
+                            matches[(i, n)].add(l)
+        if not updated:
+            break
+    return len(m) in matches[(0, 0)]
+
+
+c = 0
+for m in messages:
+    g = goodmessage(m)
+    if (g):
+        c += 1
+    print(m, g)
+
+print(c)
