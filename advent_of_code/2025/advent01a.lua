869c5cfd5a0af4c875223251a092dd84e51ec2f7
Peter Nagy <tegla@tegla.net>
2025-12-01 13:36:43 +0100

    day 1

---
+++ advent_of_code/2025/advent01a.lua
@@ -0,0 +1,18 @@
+local s = 50
+local count = 0
+for l in io.lines('/tmp/advent01.input') do
+    local d, n = l:match("(.)(.*)")
+    n = tonumber(n)
+    if d == "L" then
+        s = s - n
+    else
+        s = s + n
+    end
+    s = s % 100
+    print(d, n, s)
+    if s == 0 then
+        count = count + 1
+    end
+end
+
+print(count)
---
+++ advent_of_code/2025/advent01b.lua
@@ -0,0 +1,20 @@
+local s = 50
+local count = 0
+for l in io.lines('/tmp/advent01.input') do
+    local d, n = l:match("(.)(.*)")
+    n = tonumber(n)
+    for _ = 1, n do
+        if d == "L" then
+            s = s - 1
+        else
+            s = s + 1
+        end
+        s = s % 100
+        if s == 0 then
+            count = count + 1
+        end
+    end
+    print(d, n, s, count)
+end
+
+print(count)
